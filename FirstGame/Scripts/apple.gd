421f335941ea00443af0b0465d1de1bb52c939be
HermanoPSousa <hermanopereira.sousa@gmail.com>
2024-05-22 20:47:34 -0300

    Ajustando os platforms, aple e adicionando coins

--- FirstGame/Scripts/apple.gd
+++ FirstGame/Scripts/apple.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	$Anime.play("collect")
+	$Anime.play("collection")
 	
 
 
